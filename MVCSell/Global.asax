de357902ce31bb940fcdc8fa158e1962bef599aa
Selvabharathi <bharathikavap@gmail.com>
2022-10-17 09:57:31 +0530

    Add project files.

---
+++ MVCSell/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSell.MvcApplication" Language="C#" %>
---
+++ MVCSell/Models/lapmodel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCSell.Models
+{
+    public class lapmodel
+    {
+        [Required()]
+        public int lapid
+        {
+            get;
+            set;
+        }
+        [MaxLength(20, ErrorMessage = "Maximum 20 characters")]
+        public string lapname
+        {
+            get;
+            set;
+        }
+    }
+}
